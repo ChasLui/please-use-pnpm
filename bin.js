1d31e8efd0001668f916fd08256453b4f280ee1e
ChasLui <chaslui@outlook.com>
2022-02-08 11:10:18 +0800

    fix: 修正提示语

--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 if (!/pnpm\.js$|pnpmpkg$/.test(process.env)) {
     console.error(`\x1B[1;31m${"*".repeat(40)}\x1B[0;0m`);
-    console.error(`\x1B[1;31m* 请使用 yarn 安装依赖。\x1B[0;0m`);
+    console.error(`\x1B[1;31m* 请使用 pnpm 安装依赖。\x1B[0;0m`);
     console.error(`\x1B[1;31m${"*".repeat(40)}\x1B[0;0m`);
     console.error(``);
     process.exit(1);
